9c4b49678327cd2a2362cf05a91052a1bd4d31ea
bichsonnhat <bichsonnhat@gmail.com>
2025-06-29 01:27:13 +0700

    fix: correct current period end reference in webhook route
    - Update the reference for current period end in the webhook route to use the correct property from the subscription object.

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
         stripeCustomerId: subscription.customer as string,
         stripePriceId: subscription.items.data[0].price.id,
         stripeCurrentPeriodEnd: new Date(
-          (subscription as any).current_period_end * 1000
+          subscription.items.data[0].current_period_end * 1000
         ),
       },
     })
